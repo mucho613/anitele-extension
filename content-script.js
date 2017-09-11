c972bbff960f7efd8c91866ad03519fcef85c3ff
Daisuke Yamamura <mucho613@gmail.com>
2017-09-11 17:22:56 +0900

    ちょっと動く

--- content-script.js
+++ content-script.js
@@ -1,12 +1,65 @@
-var injectScript;
+var playButton, pauseButton, previousEpisodeButton, nextEpisodeButton, buttomControl, fullScreenButton;
 
-injectScript = function(file, node) {
-  var s, th;
-  th = document.getElementsByTagName(node)[0];
-  s = document.createElement('script');
-  s.setAttribute('type', 'text/javascript');
-  s.setAttribute('src', file);
-  return th.appendChild(s);
-};
+getDOM = function() {
+  playButton = document.getElementById('player-ctrl-play');
+  pauseButton = document.getElementById('player-ctrl-pause');
+  previousEpisodeButton = document.getElementById('player-ctrl-prev-ep');
+  nextEpisodeButton = document.getElementById('player-ctrl-next-ep');
+  fullScreenButton = document.getElementById('player-ctrl-full-screen');
+  buttonControl = document.getElementById
+}
 
-injectScript(chrome.extension.getURL('/embeded-script.js'), 'body');
+fullScreenEnable = function() {
+  // fullScreenButton.click();
+}
+
+playMovie = function() {
+  playButton.click();
+}
+
+hideUI = function() {
+  playButton.style.display = 'none';
+  pauseButton.style.display = 'none';
+  previousEpisodeButton.style.display = 'none';
+  nextEpisodeButton.style.display = 'none';
+}
+
+goPreviousEpisode = function() {
+  window.location.href = previousEpisodeButton.href;
+}
+
+goNextEpisode = function() {
+  window.location.href = nextEpisodeButton.href;
+}
+
+var movieLength;
+
+// onload で呼ぶ
+initialize = function() {
+  getDOM();
+  fullScreenEnable();
+  playMovie();
+  // 念のため存在チェック
+  if(document.getElementById('player-ctrl-movie-len') != null) {
+    movieLength = document.getElementById('player-ctrl-movie-len').textContent;
+    if(movieLength !== '00:00:00') {
+      setTimeout(hideUI, 5000);
+      endCheck();
+    }
+    else setTimeout(initialize, 5000);
+  }
+  else setTimeout(initialize, 1000);
+}
+
+endCheck = function() {
+  currentPosition = document.getElementById('player-ctrl-current-pos').textContent;
+  console.log(currentPosition);
+
+  if(movieLength === currentPosition) {
+    goNextEpisode();
+  }
+
+  setTimeout(endCheck, 1000);
+}
+
+initialize();
